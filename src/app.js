020202ea4296c4f19be3e2a0e58e50b319bfa697
Akash Vyas <akashvyas2006@gmail.com>
2026-04-04 22:09:31 +0530

    changes in CORS

--- src/app.js
+++ src/app.js
@@ -15,18 +15,18 @@ const app = express();
 // CORS configuration
 const allowedOrigins = process.env.FRONTEND_URL ? [process.env.FRONTEND_URL] : ['http://localhost:3000'];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  optionsSuccessStatus: 200
-};
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   optionsSuccessStatus: 200
+// };
 
-app.use(cors(corsOptions)); // Enable CORS with options
+app.use(cors({origin:"*"})); // Enable CORS with options
 
 // Request logging middleware
 app.use((req, res, next) => {
